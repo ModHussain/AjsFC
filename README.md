88913f99e552f542be6cf703c98854133a39ba6b
ModHussain <md.hussain.dg@gmail.com>
2020-02-27 13:24:19 +0530

    initial setup HFC

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple AFBC app
---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,23 @@
+(function() {
+  function HomeCtrl(Room, Message, $cookies) {
+    this.rooms = Room.all;
+    this.activeRoom = null;
+    this.currentUser = $cookies.get('blocChatCurrentUser');
+
+    this.changeRoom = function(room) {
+      this.activeRoom = room;
+      this.messages = Message.getByRoomId(this.activeRoom.$id);
+      console.log(this.messages);
+    };
+
+    this.sendMessage = function() {
+      Message.send(this.newMessage, this.activeRoom);
+      this.newMessage = '';
+    };
+
+  }
+
+  angular
+    .module('blocChat')
+    .controller('HomeCtrl', ['Room', 'Message', '$cookies', HomeCtrl]);
+})();
